93331d05b348dea3c423967355c2b834f553ff54
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-02-27 07:49:50 +0530

    resolve the bug related routing of home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CategoriesStoreItem } from './services/category/categories.storageItem';
 import { ProductStoreItem } from './services/product/productStoreItem';
 import { SearchKeyword } from './types/searchKeyword.type';
-
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   constructor(
     private categoriesStoreItem: CategoriesStoreItem,
-    private productStoreItem: ProductStoreItem
+    private productStoreItem: ProductStoreItem,
+    private router: Router
   ) {
     this.categoriesStoreItem.loadCategories();
     this.productStoreItem.loadProducts();
+    router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        if ((event as NavigationEnd).url == '/home') {
+          router.navigate(['/home/products']);
+        }
+      });
   }
-
- 
+  ngOnInit(): void {}
 
   onSelectCategory(mainCategoryId: number) {
     this.productStoreItem.loadProducts('mainCategoryId= ' + mainCategoryId);
